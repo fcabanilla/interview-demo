66f96143e142ea4fb20bd8e4d806644cf76ee455
Federico Cabanilla <federico.cabanilla@gmail.com>
2025-02-21 08:54:23 -0300

    Actualizar configuración de Playwright, agregar pruebas para la API de Pokémon y eliminar archivos obsoletos

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,46 @@
-import { defineConfig } from "@playwright/test";
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { Status } from "allure-js-commons";
+import * as os from "node:os";
 
-export default defineConfig({
-  use: {
-    baseURL: "https://pokeapi.co/api/v2",
-    trace: "on",
-  },
-  reporter: [["list"], ["allure-playwright"]],
-  outputDir: "allure-results",
-});
+const config: PlaywrightTestConfig = {
+  reporter: [
+    ["line"],
+    [
+      "allure-playwright",
+      {
+        resultsDir: "allure-results",
+        detail: true,
+        suiteTitle: true,
+        links: {
+          issue: {
+            nameTemplate: "Issue #%s",
+            urlTemplate: "https://issues.example.com/%s",
+          },
+          tms: {
+            nameTemplate: "TMS #%s",
+            urlTemplate: "https://tms.example.com/%s",
+          },
+          jira: {
+            urlTemplate: (v) => `https://jira.example.com/browse/${v}`,
+          },
+        },
+        categories: [
+          {
+            name: "foo",
+            messageRegex: "bar",
+            traceRegex: "baz",
+            matchedStatuses: [Status.FAILED, Status.BROKEN],
+          },
+        ],
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+        },
+      },
+    ],
+  ],
+};
+
+export default config;
